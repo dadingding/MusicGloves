3397a62d9fdb661ddc69137cebc7232a7176f8df
elega <445092967@qq.com>
2014-07-03 13:00:57 +0800

    测试一下。

--- app/src/main/java/team/dingding/musicgloves/MainActivity.java
+++ app/src/main/java/team/dingding/musicgloves/MainActivity.java
@@ -2,6 +2,7 @@ package team.dingding.musicgloves;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int b;
     }
 
 
@@ -19,6 +21,7 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        Log.v("fafaafa", "fafafafa");
         return true;
     }
 
