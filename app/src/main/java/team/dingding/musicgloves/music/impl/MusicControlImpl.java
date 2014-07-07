35eba8d14dc2e7901206cb877b2b48ffce9a082a
guoyanchang <752410335@qq.com>
2014-07-08 01:23:52 +0800

    音乐控制接口大部分都实现了，并且把MusicActivity重新写了一遍，全部改成调用接口，并且测试成功

--- app/src/main/java/team/dingding/musicgloves/music/impl/MusicControlImpl.java
+++ app/src/main/java/team/dingding/musicgloves/music/impl/MusicControlImpl.java
@@ -17,6 +17,7 @@ import team.dingding.musicgloves.music.intf.IPlayMusic;
 
 public class MusicControlImpl implements IPlayMusic {
     SoundPool soundPool;
+    SoundPool soundPool2;
     int loadcount=0;
     int currplay[];
     boolean loadsign=false;
@@ -30,6 +31,7 @@ public class MusicControlImpl implements IPlayMusic {
         for(int i=0;i<8;i++)
             currplay[i]=0;
         soundPool = new SoundPool(10, AudioManager.STREAM_SYSTEM,10);
+        soundPool2 = new SoundPool(10, AudioManager.STREAM_SYSTEM,10);
         soundMap = new HashMap<Integer, Integer>();
         am=(AudioManager) context.getSystemService(context.AUDIO_SERVICE);
     }
@@ -39,40 +41,60 @@ public class MusicControlImpl implements IPlayMusic {
         final ProgressDialog dialog=ProgressDialog.show(mContext,
                 "loading music","wait...",true);
 
-//        soundMap.put(1, soundPool.load(mContext, R.raw.doo, 1));
-//        soundMap.put(2, soundPool.load(mContext, R.raw.re, 1));
-//        soundMap.put(3, soundPool.load(mContext, R.raw.mi, 1));
-//        soundMap.put(4, soundPool.load(mContext, R.raw.fa, 1));
-//        soundMap.put(5, soundPool.load(mContext, R.raw.so, 1));
-//        soundMap.put(6, soundPool.load(mContext, R.raw.la, 1));
-//        soundMap.put(7, soundPool.load(mContext, R.raw.xi, 1));
-//        soundMap.put(8, soundPool.load(mContext, R.raw.hdo, 1));
+        soundMap.put(1, soundPool.load(mContext, R.raw.cdoo, 1));
+        soundMap.put(2, soundPool.load(mContext, R.raw.cre, 1));
+        soundMap.put(3, soundPool.load(mContext, R.raw.cmi, 1));
+        soundMap.put(4, soundPool.load(mContext, R.raw.cfa, 1));
+        soundMap.put(5, soundPool2.load(mContext, R.raw.cso, 1));
+        soundMap.put(6, soundPool2.load(mContext, R.raw.cla, 1));
+        soundMap.put(7, soundPool2.load(mContext, R.raw.cxi, 1));
+        soundMap.put(8, soundPool2.load(mContext, R.raw.chdo, 1));
 
         soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener(){
             @Override
             public void onLoadComplete(SoundPool arg0, int arg1, int arg2) {
                 loadcount = loadcount + 1;
-                if (loadcount == 8) {
+                Toast.makeText(mContext, String.valueOf(loadcount),
+                        Toast.LENGTH_SHORT).show();
+                if (loadcount == 6) {
                     dialog.dismiss();
                     loadsign=true;
                     }
+            }
+        });
+        soundPool2.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener(){
+            @Override
+            public void onLoadComplete(SoundPool arg0, int arg1, int arg2) {
+                loadcount = loadcount + 1;
+                Toast.makeText(mContext, String.valueOf(loadcount),
+                        Toast.LENGTH_SHORT).show();
+                if (loadcount == 6) {
+                    dialog.dismiss();
+                    loadsign=true;
+                }
             }
         });
         return loadsign;
     }
     //播放某个音乐
     public void play(int music){
-        currplay[music]=soundPool.play(soundMap.get(music),1,1,0,0,1);
+        if(music<=4)
+            currplay[music-1]=soundPool.play(soundMap.get(music),1,1,0,0,1);
+        else
+            currplay[music-1]=soundPool2.play(soundMap.get(music),1,1,0,0,1);
     }
     //停止播放某个音乐
     public void stop(int music){
-        soundPool.stop(music);
+        if(music<=4)
+            soundPool.stop(music);
+        else
+            soundPool2.stop(music);
     }
     //停止播放所有音乐
     public void stopAll(){
     //本来打算清理掉所有声音资源，但是考虑到声音并不多，直接一个一个关闭就行了
         for(int i=0;i<8;i++)
-            soundPool.stop(i);
+            stop(i);
     }
     //设置音量（可以先不写）
     public void setVolume(int value){
